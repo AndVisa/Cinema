e91de02878e037b25b427440b73f132b3ac82045
Andre <andre.villanueva@icloud.com>
2026-01-07 09:35:46 -0600

    Creacion de Contexto Global para la gestion de favoritos

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
+import Banner from "components/Banner";
 import Cabecera from "components/Cabecera/Cabecera";
 import Pie from "components/Pie";
+import Titulo from "components/Titulo";
+import FavoritosProvider from "context/Favoritos";
 import Container from "pages/Container";
 import Favoritos from "pages/Favoritos";
 
@@ -10,11 +13,17 @@ function AppRoutes() {
     return (
         <BrowserRouter>
             <Cabecera />
+            <Banner img="home" color="#154580" />
+            <Titulo>
+                <h1>Un lugar para guardar sus videos favoritos </h1>
+            </Titulo>
             <Container>
+                <FavoritosProvider>
                 <Routes>
                     <Route path="/" element={<Inicio />}></Route>
                     <Route path="/Favoritos" element={<Favoritos />}></Route>
                 </Routes>
+                </FavoritosProvider>
             </Container>
             <Pie />
         </BrowserRouter>
