ad9145829658bea49882ef824acdb0893730921a
Andre <iron_an_09@live.com.mx>
2026-02-02 19:49:40 -0600

    Rutas anidadas, para una mejor organizacion del codigo

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,7 @@
-import Cabecera from "components/Cabecera/Cabecera";
-import Pie from "components/Pie";
-import FavoritosProvider from "context/Favoritos";
-import Container from "components/Container";
 import Favoritos from "pages/Favoritos";
 import Player from "pages/Player";
 import NotFound from "pages/NotFound";
+import PaginaBase from "pages/PaginaBase";
 
 const { default: Inicio } = require("pages/Inicio");
 const { BrowserRouter, Routes, Route } = require("react-router-dom");
@@ -12,18 +9,14 @@ const { BrowserRouter, Routes, Route } = require("react-router-dom");
 function AppRoutes() {
     return (
         <BrowserRouter>
-            <Cabecera />
-            <Container>
-                <FavoritosProvider>
-                <Routes>
-                    <Route path="/" element={<Inicio />}></Route>
-                    <Route path="/Favoritos" element={<Favoritos />}></Route>
-                    <Route path="/:id" element={<Player />}></Route>
+            <Routes>
+                <Route path="/" element={<PaginaBase />}>
+                    <Route index element={<Inicio />}></Route>
+                    <Route path="Favoritos" element={<Favoritos />}></Route>
+                    <Route path=":id" element={<Player />}></Route>
                     <Route path="*" element={<NotFound />}></Route>
-                </Routes>
-                </FavoritosProvider>
-            </Container>
-            <Pie />
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
